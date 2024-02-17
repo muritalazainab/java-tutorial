3117e94583b6283adf70c235336570b53517ff1d
muritalazainab <muritala21@icloud.com>
2024-02-17 14:47:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// // GLOBAL and local scope VARIABLE
+
+// let i;
+// for (i = 6; i < 5; i ++) {
+//     let p = 10
+//     console.log(i + p)
+// }
+// console.log(p)
+
+// ARITHMETIC OPERATION
+
+// let cost = 15
+// let profit;
+// let salesPrice = 20
+
+// profit = salesPrice - cost;
+// console.log(profit);
+
+// plus (+) : number
+
+// let cost = 15, profit = 5, salesPrice = cost + profit 
+// console.log(salesPrice)
+
+// plus (+) : for strings
+
+let firstName = " tunde" ,surname = "zara", fullName = surname + firstName;
+
+console.log(fullName  + " is a big boy")
+
+
+
+
+
+
+
